dab9b90c5041dd2a42cab5052cd0e78a8bf1cdf7
Brett Hoerner <brett@bretthoerner.com>
2017-05-26 12:32:57 -0500

    Add to JSON.

--- sentry/src/main/java/io/sentry/jvmti/LocalsCache.java
+++ sentry/src/main/java/io/sentry/jvmti/LocalsCache.java
@@ -1,7 +1,11 @@
 package io.sentry.jvmti;
 
-public class LocalsCache {
+public final class LocalsCache {
     private static ThreadLocal<Frame[]> result = new ThreadLocal<>();
+
+    private LocalsCache() {
+
+    }
 
     public static void setCache(Frame[] frames) {
         result.set(frames);
